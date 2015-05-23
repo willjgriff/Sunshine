c916390f9a9b5c1688d33a354922af5efb02f084
willjgriff <willjgriff@gmail.com>
2015-05-23 12:43:31 +0100

    Updated layouts. Udacity lesson 1

--- app/src/main/java/com/example/will/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/will/sunshine/app/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends ActionBarActivity {
 
--- app/src/main/java/com/example/will/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/will/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,6 +23,20 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        String[] forecastData = {"Mon 6/23?- Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"};
+
+        List<String> forecastEntries = new ArrayList<String>(Arrays.asList(forecastData));
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast);
+
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
