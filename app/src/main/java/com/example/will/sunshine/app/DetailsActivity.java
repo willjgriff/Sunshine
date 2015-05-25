d71faf4f54a4a026bb21467e879c484f4c659af6
willjgriff <willjgriff@gmail.com>
2015-05-25 22:32:39 +0100

    Added share button for details fragment. Completed Lesson 3.

--- app/src/main/java/com/example/will/sunshine/app/DetailsActivity.java
+++ app/src/main/java/com/example/will/sunshine/app/DetailsActivity.java
@@ -1,13 +1,18 @@
 package com.example.will.sunshine.app;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 
 
 public class DetailsActivity extends ActionBarActivity {
+
+    ShareActionProvider shareActionProvider = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,7 @@ public class DetailsActivity extends ActionBarActivity {
         setContentView(R.layout.activity_forecast_details);
     }
 
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -22,6 +28,7 @@ public class DetailsActivity extends ActionBarActivity {
         return true;
     }
 
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
