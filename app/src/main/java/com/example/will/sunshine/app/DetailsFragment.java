2ef9a70dc3fa6f1c61cd71be7b06e2662ab885a4
willjgriff <willjgriff@gmail.com>
2015-05-27 12:42:31 +0100

    Creating databases. Near the end of Lesson 4A.

--- app/src/main/java/com/example/will/sunshine/app/DetailsFragment.java
+++ app/src/main/java/com/example/will/sunshine/app/DetailsFragment.java
@@ -3,7 +3,13 @@ package com.example.will.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,7 +20,13 @@ import android.widget.TextView;
  */
 public class DetailsFragment extends Fragment {
 
+    private static final String LOG_TAG = DetailsFragment.class.getSimpleName();
+
+    private static final String FORECAST_HASHTAG = " #SunshineApp";
+    private String forecastString;
+
     public DetailsFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -25,11 +37,36 @@ public class DetailsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_forecast_details, container, false);
 
         if (intent != null && intent.hasExtra("DAY_DETAILS")) {
-            String dayDetails = intent.getExtras().getString("DAY_DETAILS");
+            forecastString = intent.getExtras().getString("DAY_DETAILS");
             ((TextView) rootView.findViewById(R.id.textview_forecast_details))
-                    .setText(dayDetails);
+                    .setText(forecastString);
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailsfragment, menu);
+        MenuItem shareOption = menu.findItem(R.id.action_share);
+        ShareActionProvider shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(shareOption);
+
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null :( no sharing");
+        }
+
+    }
+
+    public Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, forecastString + FORECAST_HASHTAG);
+        shareIntent.setType("text/plain");
+
+        return shareIntent;
     }
 }
